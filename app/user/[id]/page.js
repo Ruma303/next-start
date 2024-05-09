926ba259f3bd3b8c2dcd17b3af99495ffea4b821
Ruma303 <matteoh8@hotmail.it>
2024-05-09 16:46:57 +0200

    dynamic route

---
+++ app/user/[id]/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center py-4">
+            <h1 className="text-4xl">User details...</h1>
+        </div>
+    )
+}
