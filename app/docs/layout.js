e2ded3580fc1a77f4ce80c47dbd781429951133a
Ruma303 <matteoh8@hotmail.it>
2024-05-11 19:03:26 +0200

    layouts.js

--- app/docs/layout.js
+++ app/docs/layout.js
@@ -3,8 +3,8 @@ import React from 'react'
 const layout = ({ children }) => {
     return (
         <div className="flex flex-col items-center py-4 w-screen">
-            <h1 className="text-2xl text-black bg-white w-screen text-center py-2">
-                Layout User</h1>
+            <h1 className="text-2xl text-white bg-blue-600 w-screen text-center py-2">
+                Layout Docs</h1>
                 {children}
         </div>
     )
