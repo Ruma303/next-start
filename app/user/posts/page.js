4a32f9307bb7ab66ca86a3aa34338265371d7aae
Ruma303 <matteoh8@hotmail.it>
2024-05-11 18:00:48 +0200

    cambio in posts

--- app/user/posts/page.js
+++ app/user/posts/page.js
@@ -1,7 +1,7 @@
-export default function User() {
+export default function Post() {
     return (
         <div className="flex flex-col items-center py-4">
-            <h1 className="text-4xl">Infos...</h1>
+            <h1 className="text-4xl">Posts .... </h1>
         </div>
     )
 }
