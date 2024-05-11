2cc73908ac542f12cff3ebc025d422dfc8187764
Ruma303 <matteoh8@hotmail.it>
2024-05-11 18:26:56 +0200

    catch all segments

--- app/user/[userId]/posts/[postId]/page.js
+++ app/user/[userId]/posts/[postId]/page.js
@@ -1,7 +1,7 @@
 export default function Post({ params }) {
     return (
         <div className="flex flex-col items-center py-4">
-            <h1 className="text-4xl">Post {params.postId}</h1>
+            <h1 className="text-4xl">Post {params.postId} of user {params.userId}</h1>
         </div>
     )
 }
