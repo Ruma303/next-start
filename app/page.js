1478bd1f12e0796f8552e2f3708efdc6adc1e44f
Ruma303 <matteoh8@hotmail.it>
2024-05-11 21:15:43 +0200

    route groups

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Navbar from "./_components/navbar";
 export default function Home() {
     return (
         <>
-            <main className="flex min-h-screen flex-col items-center p-24">
+            <div className="flex min-h-screen flex-col items-center p-24">
                 <h1 className="text-4xl">Welcome to the Next.js Course</h1>
                 <p className="text-lg">This is a simple app that demonstrates how to use Next.js</p>
-            </main>
+            </div>
         </>
     );
 }
