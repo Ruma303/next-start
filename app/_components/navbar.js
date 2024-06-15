4a1db4f3c7d4f6d22203f6ea6ebe00bd8c5e45da
Ruma303 <matteoh8@hotmail.it>
2024-06-15 15:20:44 +0200

    .

--- app/_components/navbar.js
+++ app/_components/navbar.js
@@ -1,19 +1,35 @@
+'use client';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+
+const navLink = [
+    { href: '/', label: 'Home', scroll: true, prefetch: true },
+    { href: '/user', label: 'Users' },
+    { href: '/docs', label: 'Docs', prefetch: true },
+    { href: '/register', label: 'Register' },
+    { href: '/login', label: 'Login', scroll: true },
+];
+
 export default function Navbar() {
+    const path = usePathname();
+    const router = useRouter();
     return (
-        <div className="flex flex-col items-center py-4">
-            <nav>
-                <Link href="/" className='text-md text-blue-400 underline underline-offset-2'>
-                    Home </Link>
-                <Link href="/user" className='text-md text-blue-400 underline underline-offset-2'>
-                    Users </Link>
-                <Link href="/docs" className='text-md text-blue-400 underline underline-offset-2'>
-                    Docs </Link>
-                <Link href="/register" className='text-md text-violet-400 underline underline-offset-2'>
-                    Register </Link>
-                <Link href="/login" className='text-md text-violet-400 underline underline-offset-2'>
-                    Login </Link>
+        <div className="flex flex-col items-center py-4 space-x-4">
+            <nav className='flex gap-3'>
+                {/* <button type="button" onClick={() => router.push('/dashboard')}>
+                    Dashboard
+                </button> */}
+                {navLink.map(({ href, label }) => {
+                    const isActive = path === href || path.startsWith(href + '/');
+                    return (
+                        <Link key={href} href={href} scroll={false}
+                            className={`text-xl hover:text-amber-400 underline underline-offset-2 ${isActive ? 'text-amber-400' : 'text-blue-400'}`}>
+                            {label}
+                        </Link>
+                    );
+                })}
             </nav>
         </div>
-    )
+    );
 }
