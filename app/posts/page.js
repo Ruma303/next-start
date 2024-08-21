45f9633c9f05ceb1a26044c89a3b02b71dfd36a5
Ruma303 <matteoh8@hotmail.it>
2024-08-21 18:36:05 +0200

    .

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,7 +1,17 @@
-export default function PostList({ params }) {
+export default async function Posts({ params }) {
+    const posts = await fetch('http://localhost:3000/posts').then(res => res.json());
+
     return (
-        <div className="flex flex-col items-center py-4">
-            <h1 className="text-4xl">Posts of user {params.userId}</h1>
+        <div className="flex flex-col items-center py-4 gap-4">
+            <h1 className="text-4xl">All posts from json-server</h1>
+            <div className="flex flex-wrap gap-4">
+                {posts.map(post => (
+                    <div key={post.id} className="border rounded-lg shadow-md p-4">
+                        <h2 className="text-2xl">{post.title}</h2>
+                        <p>{post.content}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
